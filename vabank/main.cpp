4e7ac25ea94b8eacedfaa68799ea88a715d93dcc
Darrmen <114183766+KarolBoro@users.noreply.github.com>
2025-06-10 18:29:24 +0200

    sa

--- vabank/main.cpp
+++ vabank/main.cpp
@@ -7,7 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     a.setWindowIcon(QIcon("Image 9 maj 2025, 09_32_38.png"));
-    //wrzucic tego file'a do folderu zasoby w projekcie
 
     MainWindow w;
     w.show();
